7c9d6f3bc05e2929daa5a62b3d1f921e4ba4fe59
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-04-22 15:09:14 +0530

    msg mulesoft

--- samplegit/src/samplegit/Welcome.java
+++ samplegit/src/samplegit/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to git desktop");
+		System.out.println("this is from mulesoft batch");  
 	}
 
 }
