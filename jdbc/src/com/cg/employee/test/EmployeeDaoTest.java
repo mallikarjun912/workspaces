c0f0d11b4dfcc9c68c0087534905d56f3da8a381
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-04-28 16:03:11 +0530

    test-2

--- jdbc/src/com/cg/employee/test/EmployeeDaoTest.java
+++ jdbc/src/com/cg/employee/test/EmployeeDaoTest.java
@@ -1,19 +1,28 @@
 package com.cg.employee.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+import org.junit.Test;
+
+//import org.junit.jupiter.api.Test;
 import com.cg.employee.beans.Employee;
 import com.cg.employee.dao.EmployeeDao;
 import com.cg.employee.dao.EmployeeDaoImpl;
 
-class EmployeeDaoTest {
+public class EmployeeDaoTest {
 	EmployeeDao dao = new EmployeeDaoImpl();
-	
+	Employee emp = new Employee(306, "testing",4100,"05-02-2012");
+
 	@Test
 	public void testAddEmployee() {
-		Employee emp = new Employee(605, "testing",4100,"05-02-2012");
 		assertEquals(1, dao.addEmployee(emp));  
+	}
+
+	@Test(expected = SQLException.class)
+	public void testSQLEXception() {
+		assertEquals(1, dao.addEmployee(emp)); 
 	}
 }
