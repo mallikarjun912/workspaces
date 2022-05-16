b9eca1f7515912643f2d4478f96b649aa0755ac9
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-05-16 14:50:27 +0530

    std

--- spring-core/src/main/java/spring/core/StudentApplication.java
+++ spring-core/src/main/java/spring/core/StudentApplication.java
@@ -8,6 +8,10 @@ public class StudentApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		Student std = ctx.getBean("student",Student.class);
+		Address addr = ctx.getBean("addr",Address.class);
+		addr.setStreet("aaaa");
+		addr.setCity("Bangalore");
+		std.setAddress(addr); 
 		System.out.println(std.getRno()+std.getStdname()+std.getGroup()+" "+
 				std.getAddress().getStreet()+" "+std.getAddress().getCity());  
 	}
