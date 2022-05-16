d1b59c6a43c43dc8914c6c79994a7a995dfc0d23
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-05-16 09:59:06 +0530

    spring-core

---
+++ spring-core/src/main/java/spring/core/Employee.java
@@ -0,0 +1,19 @@
+package spring.core;
+
+public class Employee {
+	private int code;
+	private String empname;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	
+}
---
+++ spring-core/src/main/java/spring/core/EmployeeApplication.java
@@ -0,0 +1,12 @@
+package spring.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmployeeApplication {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml"); 
+		Employee emp = ctx.getBean("emp",Employee.class); 
+		System.out.println(emp.getCode()+" "+emp.getEmpname());  
+	}
+}
