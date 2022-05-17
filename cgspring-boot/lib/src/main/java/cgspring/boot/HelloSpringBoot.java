d524007d512dae582563cf4ee46ae239555a7fcd
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-05-17 11:24:57 +0530

    spring boot gradle sample

---
+++ cgspring-boot/lib/src/main/java/cgspring/boot/HelloSpringBoot.java
@@ -0,0 +1,13 @@
+package cgspring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBoot.class, args);
+	}
+
+}
