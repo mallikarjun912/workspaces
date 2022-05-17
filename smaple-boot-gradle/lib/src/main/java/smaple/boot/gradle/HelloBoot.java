948c6381affa47c20bcfd8bf70189c0b4734f504
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-05-17 11:54:16 +0530

    spring boot sample

---
+++ smaple-boot-gradle/lib/src/main/java/smaple/boot/gradle/HelloBoot.java
@@ -0,0 +1,12 @@
+package smaple.boot.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBoot {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBoot.class, args);
+	}
+
+}
